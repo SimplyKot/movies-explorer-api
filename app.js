0fba9c28bc02e5ff80100badd668e53db08e53e5
Konstantin Meshkov <skyline@mail.ru>
2021-03-11 12:57:36 +0300

    feat: configurate express, cors, mongoose

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-// Начнем с .env
+// Подключаем файл окружения .env
 require("dotenv").config();
 
-console.log(process.env.PORT);
+const express = require("express");
+const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+const cors = require("cors");
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Вклчаем CORS
+app.use(cors());
+
+app.listen(PORT);
+// eslint-disable-next-line no-console
+console.log(`App started on port ${PORT}`);
