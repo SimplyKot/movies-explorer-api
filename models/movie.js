d5f61c3a4c0b62cdacb54fc375d5c3333e6531ef
Konstantin Meshkov <simply.kot@yandex.ru>
2021-03-13 00:27:04 +0300

    feat: add movirSchema

--- models/movie.js
+++ models/movie.js
@@ -1,3 +1,64 @@
 const mongoose = require("mongoose");
 
-const movieSchema = new mongoose.Schema({});
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, "Поле 'country' должно быть заполнено"],
+  },
+  director: {
+    type: String,
+    required: [true, "Поле 'director' должно быть заполнено"],
+  },
+  duration: {
+    type: Number,
+    required: [true, "Поле 'duration' должно быть заполнено"],
+  },
+  year: { type: Number, required: [true, "Поле 'year' должно быть заполнено"] },
+  description: {
+    type: String,
+    required: [true, "Поле 'description' должно быть заполнено"],
+  },
+  image: {
+    type: String,
+    required: [true, "Поле 'image' должно быть заполнено"],
+    validate: {
+      validator(v) {
+        const regex = /^((http|https):\/\/)?(www\.)?[a-zA-Z0-9-]{1,}\.?([a-z0-9]{1,})?\.([a-z0-9]{1,})?\.?\w{1,}?(\/([\w#!:.?+=&%@!\-/])*)?/i;
+        return regex.test(v);
+      },
+      message: "Ссылка на картинку некорректна",
+    },
+  },
+  trailer: {
+    type: String,
+    required: [true, "Поле 'trailer' должно быть заполнено"],
+    validate: {
+      validator(v) {
+        const regex = /^((http|https):\/\/)?(www\.)?[a-zA-Z0-9-]{1,}\.?([a-z0-9]{1,})?\.([a-z0-9]{1,})?\.?\w{1,}?(\/([\w#!:.?+=&%@!\-/])*)?/i;
+        return regex.test(v);
+      },
+      message: "Ссылка на трейлер некорректна",
+    },
+  },
+  nameRU: {
+    type: String,
+    required: [true, "Поле 'nameRU' должно быть заполнено"],
+  },
+  nameEN: {
+    type: String,
+    required: [true, "Поле 'nameEN' должно быть заполнено"],
+  },
+  thumbnail: {
+    type: String,
+    required: [true, "Поле 'thumbnail' должно быть заполнено"],
+    validate: {
+      validator(v) {
+        const regex = /^((http|https):\/\/)?(www\.)?[a-zA-Z0-9-]{1,}\.?([a-z0-9]{1,})?\.([a-z0-9]{1,})?\.?\w{1,}?(\/([\w#!:.?+=&%@!\-/])*)?/i;
+        return regex.test(v);
+      },
+      message: "Ссылка на миниатюру некорректна",
+    },
+  },
+});
+
+module.exports = mongoose.model("movie", movieSchema);
